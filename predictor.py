869705b7dac685300a9ee55bf0b39e89f9ea4515
daniel2835 <daniel2835@hotmail.com>
2022-06-02 16:52:50 -0500

    Created input method for date given and method for checking the weekday of such date

--- predictor.py
+++ predictor.py
@@ -1,9 +1,22 @@
 import datetime
-# import calendar
 
-# curr_date = datetime.datetime.today()
 time_now = datetime.datetime.now()
 time_str = time_now.strftime("%A, %d/%m/%Y %H:%M")
+
+
+def getWeekDay(date):
+    """Given a date in string format dd/mm/yyyy,
+        returns corresponding weekday
+    """
+    selectedDate = datetime.datetime.strptime(date, "%d/%m/%Y").strftime("%A")
+    return selectedDate
+
+
+def inputDate():
+    print("\nPlease enter the date to check restriction:")
+    print("Date format is dd/mm/yyyy")
+    inputDate = input()
+    return inputDate
 
 
 def getLastDigitLP(licensePlate):
@@ -13,7 +26,7 @@ def getLastDigitLP(licensePlate):
 
 
 def inputLicensePlate():
-    print("Please enter your car license plate")
+    print("Please enter your car license plate:")
     licensePlate = input()
     return licensePlate
 
@@ -21,18 +34,20 @@ def inputLicensePlate():
 def welcomeMessage():
     print("Welcome to Pico y Placa Predictor")
     print("This program will help you know if your car is allowed to be used during \"Pico y Placa\" restriction in Quito-Ecuador.")
-    print("Note: All dates format will be dd/mm/yyyy and hours will be 24h format")
+    print("Note: All format dates must be dd/mm/yyyy and hours will use 24h format")
     print(f"\n\nToday is {time_str}")
 
 
 def run():
-    welcomeMessage()
-    licensePlate = inputLicensePlate()
-    # print test for licensePlate:
-    print(f"License {licensePlate}")
-    licensePlateLastDigit = getLastDigitLP(licensePlate)
-    # print test for lastdigit
-    print(licensePlateLastDigit)
+    # welcomeMessage()
+    # licensePlate = inputLicensePlate()
+    # # print test for licensePlate:
+    # print(f"License {licensePlate}")
+    # licensePlateLastDigit = getLastDigitLP(licensePlate)
+    # # print test for lastdigit
+    # print(licensePlateLastDigit)
+    selDate = inputDate()
+    print(getWeekDay(selDate))
 
 
 if __name__ == '__main__':
