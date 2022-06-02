62a2db4825fbb72d870a3f4e6a7203e24b78932f
daniel2835 <daniel2835@hotmail.com>
2022-06-02 12:43:14 -0500

    Added welcomeMessage function

---
+++ predictor.py
@@ -0,0 +1,14 @@
+import datetime
+
+
+def welcomeMessage():
+    print("Welcome to Pico y Placa Predictor")
+    print("This program will help you know if your car is allowed to be used during \"Pico y Placa\" restriction in Quito-Ecuador.")
+
+
+def run():
+    welcomeMessage()
+
+
+if __name__ == '__main__':
+    run()
