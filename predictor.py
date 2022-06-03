141db33b8d0489abfe560d5c9b8c2bf67e355f67
daniel2835 <daniel2835@hotmail.com>
2022-06-03 00:16:16 -0500

    Work in progress. Creating is_restricted_day_plate function that should return True or False depending on the relation between day (input) and plate number (input).

--- predictor.py
+++ predictor.py
@@ -8,14 +8,48 @@ RESTRICTION_END1 = datetime.time(9, 30, 0)
 RESTRICTION_START2 = datetime.time(16, 0, 0)
 RESTRICTION_END2 = datetime.time(21, 0, 0)
 
+DAY_CORRESPONDING_PLATES = {"Monday": [1, 2], "Tuesday": [
+    3, 4], "Wednesday": [5, 6], "Thursday": [7, 8], "Friday": [9, 0]}
+
 WEEKDAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
 license_plate = ""
 license_plate_last_digit = 0
 
 
+def is_restricted_day_plate(day, plates):
+    """Returns True if the plate number has restriction on the corresponding weekday
+    """
+    for i in DAY_CORRESPONDING_PLATES:
+        if i == day:
+            plates_day = DAY_CORRESPONDING_PLATES.get(i)
+        else:
+            plates_day = ""
+            # print(DAY_CORRESPONDING_PLATES.get(i))
+    if plates in plates_day:
+        # print("You have restriction.")
+        return True
+    else:
+        return False
+
+
+def asdf(day, plate_number):
+    if day in DAY_CORRESPONDING_PLATES:
+        if plate_number in DAY_CORRESPONDING_PLATES.items():
+            return True
+
+
+def is_applicable_plate_day(day):
+    # for day in DAY_CORRESPONDING_PLATES:
+    #     print(day)
+    if day in DAY_CORRESPONDING_PLATES:
+        return True
+    else:
+        return False
+
+
 def is_applicable_day(day):
-    """ Returns True if
+    """ Returns True if day is weekday
     """
     if day in WEEKDAYS:
         return True
@@ -82,14 +116,19 @@ def run():
     # print(f"\nYour license plate number is {license_plate}")
     # license_plate_last_digit = get_last_digit_lp(license_plate)
     # print(f"which last digit number is {license_plate_last_digit}")
-    sel_date = input_date()
-    sel_day = get_week_day(sel_date)
-    print(
-        f"The selected date is {sel_date} and the day of the week is {sel_day}")
+    # sel_date = input_date()
+    # sel_day = get_week_day(sel_date)
+    # print(f"The selected date is {sel_date} and the day of the week is {sel_day}")
     # sel_time = input_time()
     # print(f"The selected time is {sel_time}")
 
-    print(is_applicable_day(sel_day))
+    # print(is_applicable_day(sel_day))
+
+    # print(is_applicable_plate_day("Mondays"))
+
+    # print(asdf("Monday", 2))
+
+    print(is_restricted_day_plate("Tuesday", 3))
 
 
 if __name__ == '__main__':
